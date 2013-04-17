03976fadd7559dbe651f634da9278ee40cbac2c8
nclangela <nclangela.rvr@gmail.com>
2013-04-17 10:44:44 +0800

    Adds columns to users

--- db/migrate/20130416104354_add_columns_to_users.rb
+++ db/migrate/20130416104354_add_columns_to_users.rb
@@ -1,5 +1,9 @@
-class AddYearToUsers < ActiveRecord::Migration
+class AddColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :year, :integer
+    add_column :users, :course, :string
+    add_column :users, :school, :string
+    add_column :users, :organization, :array
+    add_column :users, :category_id, :integer
   end
 end
