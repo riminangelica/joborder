cdb58337530aced683bbbd7b14b51a6c20bcc562
riminangelica <riminarchive@gmail.com>
2013-04-24 15:12:07 +0800

    Migrates image attribute to Projects, fixes database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423074438) do
+ActiveRecord::Schema.define(:version => 20130424033651) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20130423074438) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "project_status"
+    t.string   "image"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
