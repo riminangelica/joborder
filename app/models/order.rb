2e5f9409a18c12c45690ab1873e52c7c22701bdb
riminangelica <riminangelica@gmail.com>
2013-05-15 16:56:00 +0800

    Fixes welcome, home controller, and edit profile

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
-  attr_accessible :aesthetics, :category_id, :email, :first_name, :mobile, :number_of_workers, :start_date, :status, :surname, :target_date, :technicalities, :user_id
+  attr_accessible :aesthetics, :category_id, :email, :first_name, :mobile, :number_of_workers, :start_date, :status, :surname, :target_date, :technicalities, :user_id, :project_name, :project_description, :skills
+
   belongs_to :category
   belongs_to :user
   #belongs_to :project
