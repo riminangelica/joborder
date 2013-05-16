3dbee1f0196d3ecfb689395673d6d841e4a459c4
nclangela <nclangela.rvr@gmail.com>
2013-05-16 11:44:05 +0800

    Changes layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,145 @@
 //= require bootstrap_notify
 //= require twitter/bootstrap
 //= require select2
+
+
+!function( $ ){
+
+  "use strict"
+
+ /* CAROUSEL CLASS DEFINITION
+  * ========================= */
+
+  var Carousel = function (element, options) {
+    this.$element = $(element)
+    this.options = $.extend({}, $.fn.carousel.defaults, options)
+    this.options.slide && this.slide(this.options.slide)
+  }
+
+  Carousel.prototype = {
+
+    cycle: function () {
+      this.interval = setInterval($.proxy(this.next, this), this.options.interval)
+      return this
+    }
+
+  , to: function (pos) {
+      var $active = this.$element.find('.active')
+        , children = $active.parent().children()
+        , activePos = children.index($active)
+        , that = this
+
+      if (pos > (children.length - 1) || pos < 0) return
+
+      if (this.sliding) {
+        return this.$element.one('slid', function () {
+          that.to(pos)
+        })
+      }
+
+      if (activePos == pos) {
+        return this.pause().cycle()
+      }
+
+      return this.slide(pos > activePos ? 'next' : 'prev', $(children[pos]))
+    }
+
+  , pause: function () {
+      clearInterval(this.interval)
+      this.interval = null
+      return this
+    }
+
+  , next: function () {
+      if (this.sliding) return
+      return this.slide('next')
+    }
+
+  , prev: function () {
+      if (this.sliding) return
+      return this.slide('prev')
+    }
+
+  , slide: function (type, next) {
+      var $active = this.$element.find('.active')
+        , $next = next || $active[type]()
+        , isCycling = this.interval
+        , direction = type == 'next' ? 'left' : 'right'
+        , fallback  = type == 'next' ? 'first' : 'last'
+        , that = this
+
+      if (!$next.length) return
+
+      this.sliding = true
+
+      isCycling && this.pause()
+
+      $next = $next.length ? $next : this.$element.find('.item')[fallback]()
+
+      if (!$.support.transition && this.$element.hasClass('slide')) {
+        this.$element.trigger('slide')
+        $active.removeClass('active')
+        $next.addClass('active')
+        this.sliding = false
+        this.$element.trigger('slid')
+      } else {
+        $next.addClass(type)
+        $next[0].offsetWidth // force reflow
+        $active.addClass(direction)
+        $next.addClass(direction)
+        this.$element.trigger('slide')
+        this.$element.one($.support.transition.end, function () {
+          $next.removeClass([type, direction].join(' ')).addClass('active')
+          $active.removeClass(['active', direction].join(' '))
+          that.sliding = false
+          setTimeout(function () { that.$element.trigger('slid') }, 0)
+        })
+      }
+
+      isCycling && this.cycle()
+
+      return this
+    }
+
+  }
+
+
+ /* CAROUSEL PLUGIN DEFINITION
+  * ========================== */
+
+  $.fn.carousel = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('carousel')
+        , options = typeof option == 'object' && option
+      if (!data) $this.data('carousel', (data = new Carousel(this, options)))
+      if (typeof option == 'number') data.to(option)
+      else if (typeof option == 'string' || (option = options.slide)) data[option]()
+      else data.cycle()
+    })
+  }
+
+  $.fn.carousel.defaults = {
+    interval: 1000
+  }
+
+  $.fn.carousel.Constructor = Carousel
+
+
+ /* CAROUSEL DATA-API
+  * ================= */
+
+  $(function () {
+    $('body').on('click.carousel.data-api', '[data-slide]', function ( e ) {
+      var $this = $(this), href
+        , $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
+        , options = !$target.data('modal') && $.extend({}, $target.data(), $this.data())
+      $target.carousel(options)
+      e.preventDefault()
+    })
+  })
+
+}( window.jQuery );
 //= require_tree .
 
 $(document).ready(function() {
@@ -30,125 +169,6 @@ $(document).ready(function() {
             });
 });
 
-/*
-	Mosaic - Sliding Boxes and Captions jQuery Plugin
-	Version 1.0.1
-	www.buildinternet.com/project/mosaic
-
-	By Sam Dunn / One Mighty Roar (www.onemightyroar.com)
-	Released under MIT License / GPL License
-*/
-
-(function($){
-
-    if(!$.omr){
-        $.omr = new Object();
-    };
-
-    $.omr.mosaic = function(el, options){
-
-        var base = this;
-
-        // Access to jQuery and DOM versions of element
-        base.$el = $(el);
-        base.el = el;
-
-        // Add a reverse reference to the DOM object
-        base.$el.data("omr.mosaic", base);
-
-        base.init = function(){
-            base.options = $.extend({},$.omr.mosaic.defaultOptions, options);
-
-            base.load_box();
-        };
-
-        // Preload Images
-        base.load_box = function(){
-        	// Hide until window loaded, then fade in
-			if (base.options.preload){
-				$(base.options.backdrop, base.el).hide();
-				$(base.options.overlay, base.el).hide();
-
-				$(window).load(function(){
-					// IE transparency fade fix
-					if(base.options.options.animation == 'fade' && $(base.options.overlay, base.el).css('opacity') == 0 ) $(base.options.overlay, base.el).css('filter', 'alpha(opacity=0)');
-
-					$(base.options.overlay, base.el).fadeIn(200, function(){
-						$(base.options.backdrop, base.el).fadeIn(200);
-					});
-
-					base.allow_hover();
-				});
-			}else{
-				$(base.options.backdrop, base.el).show();
-				$(base.options.overlay , base.el).show();
-				base.allow_hover();
-			}
-        };
-
-        // Initialize hover animations
-        base.allow_hover = function(){
-        	// Select animation
-			switch(base.options.animation){
-
-				// Handle fade animations
-				case 'fade':
-					$(base.el).hover(function () {
-			        	$(base.options.overlay, base.el).stop().fadeTo(base.options.speed, base.options.opacity);
-			        },function () {
-			        	$(base.options.overlay, base.el).stop().fadeTo(base.options.speed, 0);
-			      	});
-
-			    	break;
-
-			    // Handle slide animations
-	      		case 'slide':
-	      			// Grab default overlay x,y position
-					startX = $(base.options.overlay, base.el).css(base.options.anchor_x) != 'auto' ? $(base.options.overlay, base.el).css(base.options.anchor_x) : '0px';
-					startY = $(base.options.overlay, base.el).css(base.options.anchor_y) != 'auto' ? $(base.options.overlay, base.el).css(base.options.anchor_y) : '0px';;
-
-			      	var hoverState = {};
-			      	hoverState[base.options.anchor_x] = base.options.hover_x;
-			      	hoverState[base.options.anchor_y] = base.options.hover_y;
-
-			      	var endState = {};
-			      	endState[base.options.anchor_x] = startX;
-			      	endState[base.options.anchor_y] = startY;
-
-					$(base.el).hover(function () {
-			        	$(base.options.overlay, base.el).stop().animate(hoverState, base.options.speed);
-			        },function () {
-			        	$(base.options.overlay, base.el).stop().animate(endState, base.options.speed);
-			      	});
-
-			      	break;
-			};
-        };
-
-        // Make it go!
-        base.init();
-    };
-
-    $.omr.mosaic.defaultOptions = {
-        animation	: 'fade',
-        speed		: 150,
-        opacity		: 1,
-        preload		: 0,
-        anchor_x	: 'left',
-        anchor_y	: 'bottom',
-        hover_x		: '0px',
-        hover_y		: '0px',
-        overlay  	: '.mosaic-overlay',	//Mosaic overlay
-		backdrop 	: '.mosaic-backdrop'	//Mosaic backdrop
-    };
-
-    $.fn.mosaic = function(options){
-        return this.each(function(){
-            (new $.omr.mosaic(this, options));
-        });
-    };
-
-})(jQuery);
 
 /**
  * bootstrap-notify.js v1.0.0
