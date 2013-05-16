2f548ecd32428123d2c20d933fccd0c850a17cae
riminangelica <riminangelica@gmail.com>
2013-05-16 11:07:19 +0800

    Fixes public profile's job request form, changes in edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,18 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, except: [:show]
   def show
   	@user = User.find_by_username(params[:username])
+  	@completed = Project.where(:user_id => @user.id,
+  		:project_status => "Completed")
+  	@inProgress = Project.where(:user_id => @user.id,
+  		:project_status => "In Progress")
+  	@projects = Project.where(:user_id => @user.id)
+  	
+  	@pending = Order.where(:user_id => @user.id,
+  		:status => "Pending")
+  	@current_orders = Order.where(:user_id => @user.id,
+  		:status => "In Progress")
+  	@completed_orders = Order.where(:user_id => @user.id,
+  		:status => "Completed")
   end
 
 end
