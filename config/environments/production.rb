b9b90eaf16d8b583aea7b9653ddd8a14cb088645
nclangela <nclangela.rvr@gmail.com>
2013-05-17 16:21:23 +0800

    Tries to fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,10 @@ Blog::Application.configure do
  :enable_starttls_auto => true
  }
  config.action_mailer.default_url_options = {:host => 'nicaminerzz.herokuapp.com'}
-
+config.action_mailer.default_url_options = { :host => 'nicaminerzz.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+      config.action_mailer.default :charset => 'utf-8'
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
