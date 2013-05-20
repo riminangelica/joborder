6fbbd27cfb6b50d9b7d24cd887c31307a38c3c06
nclangela <nclangela.rvr@gmail.com>
2013-05-20 09:04:43 +0800

    Fixes action mailer

--- config/application.rb
+++ config/application.rb
@@ -62,18 +62,3 @@ module Blog
   end
 end
 
-config.action_mailer.default_url_options = { :host => 'nicaminerzz.herokuapp.com' }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default :charset => "utf-8"
-
-ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :authentication => :plain,
-    :domain => 'gmail.com',
-    :user_name => 'nicaminerzz@gmail.com',
-    :password => 'nicaminerzz123',
-    :enable_starttls_auto => true
-}
