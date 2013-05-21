dbc12a90a4dd3c581ac6e3ba2b78e4893bfdc4cc
riminangelica <riminangelica@gmail.com>
2013-05-21 14:44:04 +0800

    Fixes landing page, view order and default avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+  	unless sign_in_count > 0
+	 		edit_user_registration_path
+	 	end
+	end
 end
