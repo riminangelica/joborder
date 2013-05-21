dffd91a328bb90a270f349354a956f11951b6292
riminangelica <riminangelica@gmail.com>
2013-05-21 16:15:27 +0800

    fixes landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-  	unless current_user.sign_in_count > 0
+  	#unless current_user.sign_in_count > 0
 	 		edit_user_registration_path
-	 	end
+	 	#end
 	end
 end
